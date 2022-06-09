77f8e160070e58cbdab1a5ae2ebf020cc35351bd
Elizabeth <elizabethtreahy@gmail.com>
2022-06-09 14:46:36 -0400

    Establish routes, controllers, models

---
+++ db/migrate/20220609182743_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :author
+      t.string :title
+      t.string :description
+      t.string :url
+      t.string :published_at
+
+      t.timestamps
+    end
+  end
+end
