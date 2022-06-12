f8cf272071a5585cc07b12025f90554785692fde
Elizabeth <elizabethtreahy@gmail.com>
2022-06-12 17:54:50 -0400

    Words on the screen and heroku

---
+++ nomad/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+    )
+
+}
+
+export default Login
