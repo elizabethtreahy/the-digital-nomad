8cf037cc3de749f0441798480102f00cb98e8823
Elizabeth <elizabethtreahy@gmail.com>
2022-06-13 08:00:33 -0400

    adding client

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,6 @@ class ArticlesController < ApplicationController
         url = "https://newsapi.org/v2/top-headlines?country=fr&apiKey=#{ENV["_FIGARO_NEWS_API_KEY"]}"
         response = RestClient.get(url)
         render json: response.body
+
     end
 end
